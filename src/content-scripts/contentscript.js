84f08e2eff4d335dded99ed9a3417ed741147a99
Jurijs Kovzels <jurijs.kovzels@outlook.com>
2019-12-11 00:29:37 +0100

    formatting files

--- src/content-scripts/contentscript.js
+++ src/content-scripts/contentscript.js
@@ -1,61 +1,61 @@
 'use strict';
 
 const displayStyles = function displayStyles(location) {
-  return new Promise((resolve) => {
-    chrome.storage.sync.get('whitelist', (result) => {
-      const arr = (result.whitelist && result.whitelist.length) ? result.whitelist : [];
-      const { origin } = location;
-      const isWhitelisted = arr.filter((url) => url === origin);
+	return new Promise((resolve) => {
+		chrome.storage.sync.get('whitelist', (result) => {
+			const arr = (result.whitelist && result.whitelist.length) ? result.whitelist : [];
+			const {origin} = location;
+			const isWhitelisted = arr.filter((url) => url === origin);
 
-      resolve(isWhitelisted.length);
-    });
-  });
+			resolve(isWhitelisted.length);
+		});
+	});
 };
 
 const initializeStylesheet = function initializeStylesheet() {
-  const styleElement = document.createElement('style');
-  styleElement.setAttribute('id', 'colorLinks');
+	const styleElement = document.createElement('style');
+	styleElement.setAttribute('id', 'colorLinks');
 
-  document.head.appendChild(styleElement);
-  chrome.storage.sync.get('color', (results) => {
-    if (results.color) {
-      styleElement.sheet.insertRule('a:visited {color: ' + results.color + ';}', 0);
-      return;
-    }
-    styleElement.sheet.insertRule('a:visited {}', 0);
-  });
+	document.head.appendChild(styleElement);
+	chrome.storage.sync.get('color', (results) => {
+		if (results.color) {
+			styleElement.sheet.insertRule('a:visited {color: ' + results.color + ';}', 0);
+			return;
+		}
+		styleElement.sheet.insertRule('a:visited {}', 0);
+	});
 };
 
 const getStyleSheet = function getStyleSheet() {
-  const styleElement = document.getElementById('colorLinks');
-  const styleSheet = (styleElement) ? styleElement.sheet : undefined;
+	const styleElement = document.getElementById('colorLinks');
+	const styleSheet = (styleElement) ? styleElement.sheet : undefined;
 
-  return styleSheet;
+	return styleSheet;
 };
 
 const setStyleSheet = function setStyleSheet(color) {
-  const styleSheet = getStyleSheet();
+	const styleSheet = getStyleSheet();
 
-  if (styleSheet) {
-    styleSheet.deleteRule(0);
-    styleSheet.insertRule('a:visited { color:' + color + '}', 0);
-    chrome.storage.sync.set({ color }, () => {});
-  }
+	if (styleSheet) {
+		styleSheet.deleteRule(0);
+		styleSheet.insertRule('a:visited { color:' + color + '}', 0);
+		chrome.storage.sync.set({color}, () => {});
+	}
 };
 
 const colorListener = function colorListener(request, sender, sendResponse) { // eslint-disable-line no-unused-vars
-  const color = request.color;
+	const color = request.color;
 
-  if (color) {
-    setStyleSheet(color);
-  }
+	if (color) {
+		setStyleSheet(color);
+	}
 };
 
 displayStyles(window.location)
-  .then(urls => {
-    if (!urls) return;
+	.then(urls => {
+		if (!urls) return;
 
-    initializeStylesheet();
-    chrome.runtime.onMessage.addListener(colorListener);
-  })
-  .catch(err => console.log(err));
+		initializeStylesheet();
+		chrome.runtime.onMessage.addListener(colorListener);
+	})
+	.catch(err => console.log(err));
